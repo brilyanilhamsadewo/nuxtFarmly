baab1e3bc5de160c57bae00c0a9316d2c4baadc1
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2023-04-25 21:57:49 +0700

    feat: change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
+  target: 'static',
   postcss: {
     plugins: {
       tailwindcss: {},
